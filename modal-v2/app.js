e1e3e5755c17785fa682a8d9315757774222fe1c
burak <Reayhs1@gmail.com>
2023-01-19 21:22:05 +0300

    modal-v2

---
+++ modal-v2/app.js
@@ -0,0 +1,24 @@
+document.getElementById("heading1").addEventListener("click", function(){
+    let u = document.getElementById("para1");
+    if (u.style.display === "none") {
+        u.style.display = "block";
+    } else {
+        u.style.display = "none";
+    }
+});
+document.getElementById("heading2").addEventListener("click", function(){
+    let u = document.getElementById("para2");
+    if (u.style.display === "none") {
+        u.style.display = "block";
+    } else {
+        u.style.display = "none";
+    }
+});
+document.getElementById("heading3").addEventListener("click", function(){
+    let u = document.getElementById("para3");
+    if (u.style.display === "none") {
+        u.style.display = "block";
+    } else {
+        u.style.display = "none";
+    }
+});
